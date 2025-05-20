66e7ad0523296e88fbafddfea4005ea012652dc1
xayuzaii <wizardcodingmachine@gmail.com>
2025-05-20 17:29:49 -0600

    first commit

---
+++ src/app/pages/book-ticket/book-ticket.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-ticket',
+  imports: [],
+  templateUrl: './book-ticket.component.html',
+  styleUrl: './book-ticket.component.css'
+})
+export class BookTicketComponent {
+
+}
---
+++ src/app/pages/bookings-list/bookings-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookings-list',
+  imports: [],
+  templateUrl: './bookings-list.component.html',
+  styleUrl: './bookings-list.component.css'
+})
+export class BookingsListComponent {
+
+}
